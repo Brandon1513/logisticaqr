fdbc45539f8795c965282ceb8d76123d65301065
Nesz-ux <nhernandez1902@gmail.com>
2024-09-24 15:52:38 -0600

    Ubicaciones actualizadas, generacion de qr con exportacion de imagen

---
+++ src/assets/Ubicaciones.js
@@ -0,0 +1,88 @@
+export const ubicaciones = [
+  { value: "almacen", label: "Almacén" },
+  { value: "comedor", label: "Comedor" },
+  { value: "cocineta", label: "Cocineta" },
+  { value: "cuarto-hidro", label: "Cuarto Hidroneumático" },
+  { value: "enfermeria", label: "Enfermería" },
+  { value: "instalaciones", label: "Instalaciones" },
+  { value: "oficinas", label: "Oficinas" },
+  { value: "produccion", label: "Producción" },
+  { value: "recepcion", label: "Recepción" },
+  { value: "sanitario", label: "Sanitario" },
+  { value: "taller-mantenimiento", label: "Taller de Mantenimiento" },
+];
+
+export const produccion = [
+  { value: "aduana", label: "Aduana" },
+  { value: "envasado", label: "Envasado" },
+  { value: "envasado-barras", label: "Envasado Barras" },
+  { value: "Formado de barras", label: "Formado Barras" },
+  { value: "hornos", label: "Hornos" },
+  { value: "mezclado", label: "Mezclado" },
+];
+
+export const almacen = [
+  { value: "materia-prima", label: "Materia Prima" },
+  { value: "producto-terminado", label: "Producto Terminado" },
+  { value: "laboratorio", label: "Laboratorio" },
+];
+
+export const baños = [
+  { value: "oficinas", label: "Oficinas" },
+  { value: "produccion", label: "Producción" },
+];
+
+export const oficinas = [
+  { value: "administracion", label: "Administración" },
+  { value: "operaciones", label: "Operaciones" },
+];
+
+
+//Mapeo de variables para mostrar en frontend
+export const tipoMap = {
+  "equipo-computo": "Equipo de Cómputo",
+  "electrodomestico": "Electrodoméstico",
+  "muebles": "Muebles",
+  "maquinaria": "Maquinaria",
+  "herramienta": "Herramienta",
+};
+
+export const ubicacionesMap = {
+    "almacen": "Almacén",
+    "comedor": "Comedor",
+    "cocineta": "Cocineta",
+    "cuarto-hidro": "Cuarto Hidroneumático",
+    "enfermeria": "Enfermería",
+    "instalaciones": "Instalaciones",
+    "oficinas": "Oficinas",
+    "produccion": "Producción",
+    "recepcion": "Recepción",
+    "sanitario": "Sanitario",
+    "taller-mantenimiento": "Taller de Mantenimiento",
+  };
+  
+
+export const produccionMap = {
+  "aduana": "Aduana",
+  "envasado": "Envasado",
+  "envasado-barras": "Envasado Barras",
+  "formado de barras": "Formado Barras",
+  "hornos": "Hornos",
+  "mezclado": "Mezclado",
+};
+
+export const almacenMap = {
+  "materia-prima": "Materia Prima",
+  "producto-terminado": "Producto Terminado",
+  "laboratorio": "Laboratorio",
+};
+
+export const sanitariosMap = {
+  "oficinas": "Oficinas",
+  "produccion": "Producción",
+};
+
+export const oficinasMap = {
+  "administracion": "Administración",
+  "operaciones": "Operaciones",
+};
--- src/pages/QRGenerator.js
+++ src/pages/QRGenerator.js
@@ -1,150 +1,404 @@
-import React, { useState, useRef } from 'react';
-import { QRCodeCanvas } from 'qrcode.react'; 
-import '../assets/styles/QRGenerator.css'; 
+import React, { useState, useRef } from "react";
+import { QRCodeCanvas } from "qrcode.react";
+import "../assets/styles/QRGenerator.css";
+import {
+  ubicaciones,
+  produccion,
+  almacen,
+  baños,
+  oficinas,
+  tipoMap,
+  ubicacionesMap,
+  produccionMap,
+  almacenMap,
+  sanitariosMap,
+  oficinasMap,
+} from "../assets/Ubicaciones";
 
 function QRForm() {
-    const [formData, setFormData] = useState({
-        nombre: '',
-        noSerie: '',
-        tipo: '',
-        ubicacion: '',
-        propietario: ''
+  const [formData, setFormData] = useState({
+    nombre: "",
+    noSerie: "",
+    proveedor: "", 
+    estado: "Activo", 
+    referencia: "Dasavena2024", 
+    tipo: "",
+    ubicacion: "",
+    propietario: "",
+    ubicacionProd: "",
+    ubicacionAlma: "",
+    ubicacionSanita: "",
+    ubicacionOfi: "",
+  });
+  const [qrData, setQrData] = useState("");
+  const qrRef = useRef();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const qrString = `
+      Nombre: ${formData.nombre}\n
+      No. de Serie: ${formData.noSerie}\n
+      Proveedor: ${formData.proveedor}\n
+      Tipo: ${tipoMap[formData.tipo] || formData.tipo}\n
+      ${
+        formData.tipo === "equipo-computo"
+          ? `Propietario: ${formData.propietario}\n`
+          : ""
+      }
+      Ubicación: ${ubicacionesMap[formData.ubicacion] || formData.ubicacion}\n
+      ${
+        formData.ubicacion === "produccion"
+          ? `Producción: ${
+              produccionMap[formData.ubicacionProd] || formData.ubicacionProd
+            }\n`
+          : ""
+      }
+      ${
+        formData.ubicacion === "almacen"
+          ? `Almacén: ${
+              almacenMap[formData.ubicacionAlma] || formData.ubicacionAlma
+            }\n`
+          : ""
+      }
+      ${
+        formData.ubicacion === "sanitario"
+          ? `Sanitario: ${
+              sanitariosMap[formData.ubicacionSanita] ||
+              formData.ubicacionSanita
+            }\n`
+          : ""
+      }
+      ${
+        formData.ubicacion === "oficinas"
+          ? `Oficina: ${
+              oficinasMap[formData.ubicacionOfi] || formData.ubicacionOfi
+            }\n`
+          : ""
+      }
+    `;
+
+    setQrData(qrString.trim());
+    console.log(formData);
+  };
+
+  const handleClear = () => {
+    setFormData({
+      nombre: "",
+      noSerie: "",
+      proveedor: "", 
+      estado: "Activo", 
+      referencia: "Dasavena2024", 
+      tipo: "",
+      ubicacion: "",
+      propietario: "",
+      ubicacionProd: "",
+      ubicacionAlma: "",
+      ubicacionSanita: "",
+      ubicacionOfi: "",
     });
-    const [qrData, setQrData] = useState('');
-    const qrRef = useRef(); // Usamos useRef para acceder al canvas del QR
+    setQrData("");
+  };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const handleExport = () => {
+    const canvas = qrRef.current.querySelector("canvas");
+  
+    const exportCanvas = document.createElement("canvas");
+    const ctx = exportCanvas.getContext("2d");
+  
+    const exportWidth = canvas.width + 10;
+    const exportHeight = canvas.height + 120;
+    exportCanvas.width = exportWidth;
+    exportCanvas.height = exportHeight;
+  
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, exportWidth, exportHeight);
+  
+    ctx.fillStyle = "black";
+    ctx.font = "bold 20px Arial";
+    ctx.textAlign = "center";
+  
+    const title = "Dasavena";
+    const reference = "Referencia";
+  
+    const titleYPosition = 30;
+    const referenceYPosition = titleYPosition + 30;
+  
+    ctx.fillText(title, exportWidth / 2, titleYPosition);
+    ctx.font = "20px Arial";
+    ctx.fillText(reference, exportWidth / 2, referenceYPosition);
+  
+    const qrYPosition = referenceYPosition + 20;
+    const qrXPosition = (exportWidth - canvas.width) / 2;
+    ctx.drawImage(canvas, qrXPosition, qrYPosition);
+  
+    const exportPngUrl = exportCanvas.toDataURL("image/png");
+  
+    const downloadLink = document.createElement("a");
+    downloadLink.href = exportPngUrl;
+    downloadLink.download = `QR_${formData.nombre}_${formData.ubicacion}.png`;
+    downloadLink.click();
+  };
+  
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const qrString = `
-            Nombre: ${formData.nombre}\n
-            No. de Serie: ${formData.noSerie}\n
-            Tipo: ${formData.tipo}\n
-            Ubicación: ${formData.ubicacion}\n
-            ${formData.tipo === 'equipo-computo' ? `Propietario: ${formData.propietario}\n` : ''}
-        `;
 
-        setQrData(qrString.trim());
-        console.log(formData);
-    };
-
-    const handleClear = () => {
-        setFormData({
-            nombre: '',
-            noSerie: '',
-            tipo: '',
-            ubicacion: '',
-            propietario: ''
-        });
-        setQrData('');
-    };
-
-    const handleExport = () => {
-        const canvas = qrRef.current.querySelector('canvas'); 
-        const pngUrl = canvas.toDataURL('image/png'); 
-        const downloadLink = document.createElement('a'); 
-        downloadLink.href = pngUrl;
-        downloadLink.download = `codigoQR_${formData.nombre || 'qr'}.png`; 
-        downloadLink.click(); 
-    };
-
-    return (
-        <div className="form-container">
-            <h2>Generar Código QR</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="input-group">
-                    <label htmlFor="nombre">Nombre:</label>
-                    <input
-                        type="text"
-                        id="nombre"
-                        name="nombre"
-                        value={formData.nombre}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="noSerie">No. de Serie:</label>
-                    <input
-                        type="text"
-                        id="noSerie"
-                        name="noSerie"
-                        value={formData.noSerie}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="tipo">Tipo:</label>
-                    <select
-                        id="tipo"
-                        name="tipo"
-                        value={formData.tipo}
-                        onChange={handleChange}
-                        required
-                    >
-                        <option value="">Selecciona el tipo</option>
-                        <option value="equipo-computo">Equipo de Cómputo</option>
-                        <option value="electrodomestico">Electrodoméstico</option>
-                        <option value="muebles">Muebles</option>
-                        <option value="maquinaria">Maquinaria</option>
-                        <option value="herramienta">Herramienta</option>
-                    </select>
-                </div>
-
-                {formData.tipo === 'equipo-computo' && (
-                    <div className="input-group">
-                        <label htmlFor="propietario">Nombre del Propietario:</label>
-                        <input
-                            type="text"
-                            id="propietario"
-                            name="propietario"
-                            value={formData.propietario}
-                            onChange={handleChange}
-                            required={formData.tipo === 'equipo-computo'}
-                        />
-                    </div>
-                )}
-
-                <div className="input-group">
-                    <label htmlFor="ubicacion">Ubicación:</label>
-                    <select
-                        id="ubicacion"
-                        name="ubicacion"
-                        value={formData.ubicacion}
-                        onChange={handleChange}
-                        required
-                    >
-                        <option value="">Selecciona la ubicación</option>
-                        <option value="almacen-mp">Almacén MP</option>
-                        <option value="almacen-pt">Almacén PT</option>
-                        <option value="produccion">Producción</option>
-                        <option value="oficinas">Oficinas</option>
-                    </select>
-                </div>
-                <div className="button-group">
-                    <button type="submit" className="submit-button">Generar QR</button>
-                    <button type="button" className="clear-button" onClick={handleClear}>Limpiar Formulario</button>
-                </div>
-            </form>
-
-            {qrData && (
-                <div className="qr-code-container">
-                    <h3>Código QR generado:</h3>
-                    <div ref={qrRef}> 
-                        <QRCodeCanvas value={qrData} size={256} />
-                    </div>
-                    <pre>{qrData}</pre>
-                    
-                    <button className="export-button" onClick={handleExport}>Exportar QR como PNG</button>
-                </div>
-            )}
+  return (
+    <div className="form-container">
+      <h2>Generar Código QR</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="nombre">Nombre:</label>
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            value={formData.nombre}
+            onChange={handleChange}
+            required
+          />
         </div>
-    );
+        <div className="input-group">
+          <label htmlFor="noSerie">No. de Serie:</label>
+          <input
+            type="text"
+            id="noSerie"
+            name="noSerie"
+            value={formData.noSerie}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="proveedor">Proveedor:</label>
+          <input
+            type="text"
+            id="proveedor"
+            name="proveedor"
+            value={formData.proveedor}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="tipo">Tipo:</label>
+          <select
+            id="tipo"
+            name="tipo"
+            value={formData.tipo}
+            onChange={handleChange}
+            required
+          >
+            <option value="">Selecciona el tipo</option>
+            <option value="equipo-computo">Equipo de Cómputo</option>
+            <option value="electrodomestico">Electrodoméstico</option>
+            <option value="muebles">Muebles</option>
+            <option value="maquinaria">Maquinaria</option>
+            <option value="herramienta">Herramienta</option>
+          </select>
+        </div>
+
+        {formData.tipo === "equipo-computo" && (
+          <div className="input-group">
+            <label htmlFor="propietario">Nombre del Propietario:</label>
+            <input
+              type="text"
+              id="propietario"
+              name="propietario"
+              value={formData.propietario}
+              onChange={handleChange}
+              required={formData.tipo === "equipo-computo"}
+            />
+          </div>
+        )}
+
+        <div className="input-group">
+          <label htmlFor="ubicacion">Ubicación:</label>
+          <select
+            id="ubicacion"
+            name="ubicacion"
+            value={formData.ubicacion}
+            onChange={handleChange}
+            required
+          >
+            <option>Selecciona la ubicación</option>
+            {ubicaciones.map((ubicacion) => (
+              <option key={ubicacion.value} value={ubicacion.value}>
+                {ubicacion.label}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {formData.ubicacion === "produccion" && (
+          <div className="input-group">
+            <label htmlFor="ubicacionProd">Ubicación en Producción</label>
+            <select
+              id="ubicacionProd"
+              name="ubicacionProd"
+              value={formData.ubicacionProd}
+              onChange={handleChange}
+              required
+            >
+              <option></option>
+              {produccion.map((ubicacionProd) => (
+                <option key={ubicacionProd.value} value={ubicacionProd.value}>
+                  {ubicacionProd.label}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        {formData.ubicacion === "almacen" && (
+          <div className="input-group">
+            <label htmlFor="ubicacionAlma">Ubicación en Almacén</label>
+            <select
+              id="ubicacionAlma"
+              name="ubicacionAlma"
+              value={formData.ubicacionAlma}
+              onChange={handleChange}
+              required
+            >
+              <option></option>
+              {almacen.map((ubicacionAlma) => (
+                <option key={ubicacionAlma.value} value={ubicacionAlma.value}>
+                  {ubicacionAlma.label}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        {formData.ubicacion === "sanitario" && (
+          <div className="input-group">
+            <label htmlFor="ubicacionSanita">Ubicación del Sanitario</label>
+            <select
+              id="ubicacionSanita"
+              name="ubicacionSanita"
+              value={formData.ubicacionSanita}
+              onChange={handleChange}
+              required
+            >
+              <option></option>
+              {baños.map((ubicacionSanita) => (
+                <option
+                  key={ubicacionSanita.value}
+                  value={ubicacionSanita.value}
+                >
+                  {ubicacionSanita.label}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        {formData.ubicacion === "oficinas" && (
+          <div className="input-group">
+            <label htmlFor="ubicacionOfi">Ubicación en Oficina</label>
+            <select
+              id="ubicacionOfi"
+              name="ubicacionOfi"
+              value={formData.ubicacionOfi}
+              onChange={handleChange}
+              required
+            >
+              <option></option>
+              {oficinas.map((ubicacionOfi) => (
+                <option key={ubicacionOfi.value} value={ubicacionOfi.value}>
+                  {ubicacionOfi.label}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        <div className="button-group">
+          <button type="submit" className="submit-button">
+            Generar QR
+          </button>
+
+          <button type="button" className="clear-button" onClick={handleClear}>
+            Limpiar Formulario
+          </button>
+        </div>
+      </form>
+
+      {qrData && (
+        <div className="qr-code-container">
+          <h3>Código QR generado:</h3>
+          <div className="qr-code" ref={qrRef}>
+            <QRCodeCanvas value={qrData} size={256} />
+          </div>
+
+          <div className="qr-data-summary">
+            <h4>Resumen de Datos:</h4>
+            <p>
+              <strong>Nombre:</strong> {formData.nombre}
+            </p>
+            <p>
+              <strong>No. de Serie:</strong> {formData.noSerie}
+            </p>
+            <p>
+              <strong>Proveedor:</strong> {formData.proveedor}
+            </p>
+            <p>
+              <strong>Tipo:</strong> {tipoMap[formData.tipo] || formData.tipo}
+            </p>
+            <p>
+              <strong>Ubicación:</strong>{" "}
+              {ubicacionesMap[formData.ubicacion] || formData.ubicacion}
+            </p>
+
+            {formData.tipo === "equipo-computo" && (
+              <p>
+                <strong>Propietario:</strong> {formData.propietario}
+              </p>
+            )}
+
+            {formData.ubicacion === "produccion" && (
+              <p>
+                <strong>Ubicación en Producción:</strong>{" "}
+                {produccionMap[formData.ubicacionProd] ||
+                  formData.ubicacionProd}
+              </p>
+            )}
+
+            {formData.ubicacion === "almacen" && (
+              <p>
+                <strong>Ubicación en Almacén:</strong>{" "}
+                {almacenMap[formData.ubicacionAlma] || formData.ubicacionAlma}
+              </p>
+            )}
+
+            {formData.ubicacion === "sanitario" && (
+              <p>
+                <strong>Ubicación del Sanitario:</strong>{" "}
+                {sanitariosMap[formData.ubicacionSanita] ||
+                  formData.ubicacionSanita}
+              </p>
+            )}
+
+            {formData.ubicacion === "oficinas" && (
+              <p>
+                <strong>Ubicación Oficina:</strong>{" "}
+                {oficinasMap[formData.ubicacionOfi] || formData.ubicacionOfi}
+              </p>
+            )}
+          </div>
+
+          <button className="export-button" onClick={handleExport}>
+            Exportar QR como PNG
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default QRForm;
