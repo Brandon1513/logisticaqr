6755e4563d3b70dde3d837f0246a53029e056e95
Nesz-ux <nhernandez1902@gmail.com>
2024-10-16 12:04:18 -0600

    Configuracion para cambios locales

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = 'https://qrback.domcloud.dev';
+export const API_BASE_URL = 'https://qrback.domcloud.dev'; //Apuntador a backend web
+
+export const API_BASE_URLLocal = 'http://192.168.1.109:5000'; //Modificar dependiendo tu IP local para pruebas
